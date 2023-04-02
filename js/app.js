90c24dc1e2df733b45ba5ad9e83032f502872998
Nick Wooten <nswooten@gmail.com>
2023-04-02 10:44:21 -0500

    impliment scaleable conversion of sqId to integer

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 console.log("sanity check")
 /*-------------------------------- Constants --------------------------------*/
-const snake = {
-  head: 1,
-  body: [],
-}
+// const snake = {
+//   head: boardSqs[18],
+//   body: [],
+// }
 
 const apple = {
   location: 2,
@@ -27,30 +27,29 @@ const keyboard = document.querySelector(".keyboard")
 
 const boardSqs = document.querySelectorAll(".sqr") 
 
-
-
 /*----------------------------- Event Listeners -----------------------------*/
 
 keyboard.addEventListener("keydown", moveHead)
 
-
-
-boardSqs.forEach(function(index){
-  index = null
-  console.log(index)
-  
-})
-
-
-
-
-
 /*-------------------------------- Functions --------------------------------*/
 
+//create a function that takes the index and assigns that index number as a value to that square
+const boardSqsValue = boardSqs.forEach(function(sqr, index){
+  const num = parseInt(index)
+  sqr = num
+  return sqr
+  
+})
+console.log(boardSqs)
+console.log(boardSqsValue)
 
+// console.log(boardSqsValue())
 
 function init(){
+ 
   boardSqs[18].textContent = "x"
+  console.log(parseInt((boardSqs[18].id.replace("sq", ""))) + 3) //now i need to convert back into an index after changing the input
+  
 }
 
 function updateBoard(){
@@ -70,28 +69,37 @@ function updateSnake(){
 // }
 
 
+// function initialBoard(){
+//   boardSqs.forEach(function(sqr, index){
+//   sqr = null
+//   console.log(boardSqs[index])
+  
+// })
+// }
+// initialBoard()
 
+// function updateBoard(){
+//   console.log(boardSqs)
+  
+//   boardSqs.forEach(function(sqr, index){
+//     if(sqr === null){
+//       boardSqs[index].textContent = "x"
+//     }else if (sqr === 1){
+//       boardSqs[index].textContent = "o"
+//     }else if (sqr === 2){
+//       boardSqs[index].textContent = "a"
+//     }
+//   })
+// }
 
-function updateBoard(){
-  boardSqs.forEach(function(sqr, index){
-    if(sqr === null){
-      boardSqs[index].textContent = "x"
-    }else if (sqr === 1){
-      boardSqs[index].textContent = "o"
-    }else if (sqr === 2){
-      boardSqs[index].textContent = "a"
-    }
-  })
-}
-updateBoard()
-// ,null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
+console.log(updateBoard())
 
 // board.x1.splice(0, 1, snake.head)
 // let timer = setInterval(function(){
 //   moveHead()
 //   updateBoard()
 // }, 300)
-
+//made the x move accross the screen by pop() last value storing it as a variable and then unshifting that variable while running update board after time interval
 
 
 
@@ -101,7 +109,7 @@ function moveHead(event){
   let keyToNumber = 0
   // console.log(keyToNumber)
   if(userKey === "w"){
-    console.log("-5")
+    // console.log("-5")
     keyToNumber = -6
     
   //need to shift the boardSqs[] -5
@@ -122,7 +130,7 @@ function moveHead(event){
     //need to shift the boardSqs[] -1
   } return console.log(keyToNumber)
 }
-console.log(moveHead)
+// console.log(moveHead())
 
 
 function keyPress(event){
