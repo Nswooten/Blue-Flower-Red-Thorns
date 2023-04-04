2461dafb7c12637332f344f1c4ef3d56200461b2
Nick Wooten <nswooten@gmail.com>
2023-04-04 13:03:57 -0500

    fix reset bug

--- js/app.js
+++ js/app.js
@@ -54,6 +54,7 @@ function init(){
   apple.location = 9
   snake.body = []
   lose = false
+  currentDirection = null
   updateBoard()
   checkForLoss()
 }
@@ -81,84 +82,94 @@ function updateBoard(){
 function start(direction){
   if(direction === -6)
   timer = setInterval(function(){
-  snake.head -= 6
-  updateApple()
-  checkForWall(-6)
-  dealWithSnakeBody(-6)
-  checkForBody()
-  updateBoard()
+    snake.head -= 6
+    checkForWall(-6)
+    dealWithSnakeBody(-6)
+    checkForBody()
+    updateApple()
+    updateBoard()
   }, 300)
   if(direction === 6)
+
   timer = setInterval(function(){
-  snake.head += 6
-  updateApple()
-  checkForWall(6)
-  dealWithSnakeBody(6)
-  checkForBody()
-  updateBoard()
+    snake.head += 6
+    checkForWall(6)
+    dealWithSnakeBody(6)
+    checkForBody()
+    updateApple()
+    updateBoard()
   }, 300)
   if(direction === -1)
+
   timer = setInterval(function(){
-  snake.head -= 1
-  updateApple()
-  checkForWall(-1)
-  dealWithSnakeBody(-1)
-  checkForBody()  
-  updateBoard()
+    snake.head -= 1
+    checkForWall(-1)
+    dealWithSnakeBody(-1)
+    checkForBody()  
+    updateApple()
+    updateBoard()
   }, 300)
   if(direction === 1)
+
   timer = setInterval(function(){
-  snake.head += 1
-  updateApple()
-  checkForWall(1)
-  dealWithSnakeBody(1)
-  checkForBody()
-  updateBoard()
+    snake.head += 1
+    checkForWall(1)
+    dealWithSnakeBody(1)
+    checkForBody()
+    updateApple()
+    updateBoard()
   }, 300)
   
 }
 
 function moveHead(key){  
-  if(key.toLowerCase() === "w" && currentDirection !== "s" ){
+  if(key.toLowerCase() === "w" && currentDirection !== "s" && currentDirection !== "w" && lose === false){
     clearInterval(timer)
     currentDirection = "w"
-    console.log(currentDirection)
-    // snake.head = snake.head - 6//if i remove this it will fix the ability to move around at your own speed
+    snake.head = snake.head - 6
+    checkForWall(-6)
     start(-6)
-  }else if(key.toLowerCase() === "s" && currentDirection !== "w" ){
+    dealWithSnakeBody(-6)
+
+  }else if(key.toLowerCase() === "s" && currentDirection !== "w" && currentDirection !== "s" && lose === false ){
     clearInterval(timer)
     currentDirection = "s"
-    // snake.head = snake.head + 6
+    snake.head = snake.head + 6
+    checkForWall(6)
     start(6)
+    dealWithSnakeBody(6)
     //boardSqs[index] = (boardSqs[index].id.replace("sq", ""))) + 6 
     //need to shift the boardSqs[] +6
-  }else if(key.toLowerCase() === "d" && currentDirection !== "a"){
+  }else if(key.toLowerCase() === "d" && currentDirection !== "a" && currentDirection !== "d" && lose === false){
     clearInterval(timer)
     currentDirection = "d"
-    // snake.head = snake.head + 1
+    snake.head = snake.head + 1
+    checkForWall(1)
     start(1)
-  }else if(key.toLowerCase() === "a" && currentDirection !== "d"){
+    dealWithSnakeBody(1)
+  }else if(key.toLowerCase() === "a" && currentDirection !== "d" && currentDirection !== "a" && lose === false){
     clearInterval(timer)
     currentDirection = "a"
-    // snake.head = snake.head - 1
+    snake.head = snake.head - 1
+    checkForWall(-1)
     start(-1)
+    dealWithSnakeBody(-1)
     
   } 
-  console.log(currentDirection)
+  updateApple()
   updateBoard()
-  start()
 }
 function checkForWall(direction){
-  if(walls.bWall.includes(snake.head) && direction === 6){
+  if(walls.bWall.includes(snake.head - 6) && direction === 6){
     clearInterval(timer)
     lose = true
-  }else if(walls.rWall.includes(snake.head) && direction === 1){
+  }else if(walls.rWall.includes(snake.head - 1) && direction === 1){
     clearInterval(timer)
     lose = true
-  }else if(walls.tWall.includes(snake.head) && direction === -6){
+  }else if(walls.tWall.includes(snake.head + 6) && direction === -6){
     clearInterval(timer)
     lose = true
-  }else if(walls.lWall.includes(snake.head) && direction === -1){
+  }else if(walls.lWall.includes(snake.head + 1) && direction === -1){
     clearInterval(timer)
     lose = true
   }
